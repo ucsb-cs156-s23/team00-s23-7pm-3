0401e09211baf993e7805881d4a0f9ea8f28aeec
Yiu-Shing Li <li12@umail.ucsb.edu>
2023-04-12 19:31:22 -0700

    yl - add page for San Jose

---
+++ frontend/src/main/pages/SanJosePage.js
@@ -0,0 +1,17 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanJosePage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Jose, CA</h1>
+        <p>
+         <b>San Jose, California</b> is a town in Santa Clara county.
+         It is known for being the center of Silicon Valley, where there
+         is a great focus on technological innovation.
+        </p>
+        <p>Added by Yiu-Shing L.</p>
+      </div>
+    </BasicLayout>
+  )
+}
